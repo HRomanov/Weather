00d2474bcf04e3a55c861db40da95591b7d38106
hromanov <gennady.romanow@gmail.com>
2016-04-06 17:12:19 +0300

    Added toolbar and search button

---
+++ Weather/app/src/main/java/com/romanov/weather/MainActivity.java
@@ -0,0 +1,37 @@
+package com.romanov.weather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        initToolbar ();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
+    }
+}
+
+
+
+
