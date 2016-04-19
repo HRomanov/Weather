515d0c564a2f49932822c16c6a42b814a7cc0969
hromanov <gennady.romanow@gmail.com>
2016-04-19 20:48:17 +0300

    Implemented navigation view and tab navigation with test fragment

--- Weather/app/src/main/java/com/romanov/weather/MainActivity.java
+++ Weather/app/src/main/java/com/romanov/weather/MainActivity.java
@@ -1,22 +1,39 @@
 package com.romanov.weather;
 
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewParent;
+
+import com.romanov.weather.adapter.TabsPagerFragmentAdapter;
 
 public class MainActivity extends AppCompatActivity{
 
+    private static final int LAYOUT =R.layout.activity_main;
+
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private ViewPager viewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault); // используем свой стиль по умолчанию
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        initToolbar ();
+        setContentView(LAYOUT);
+
+        initToolbar();
+        initNavigationView();
+        initTabs();
     }
+
     private void initToolbar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.app_name);
@@ -28,6 +45,40 @@ public class MainActivity extends AppCompatActivity{
         });
 
         toolbar.inflateMenu(R.menu.menu);
+    }
+
+    private void initTabs() {
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        TabsPagerFragmentAdapter adapter = new TabsPagerFragmentAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        TabLayout tabLayout =(TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.view_navigatoin_open, R.string.view_navigatoin_close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        assert navigationView != null;
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                drawerLayout.closeDrawers();
+                switch (menuItem.getItemId()){
+                    case R.id.actionNotificationItem:
+                        showNotificationTab();
+                }
+                return true;
+            }
+        });
+    }
+
+    private void showNotificationTab(){
+        viewPager.setCurrentItem(Constants.TAB_TWO);
     }
 }
 
---
+++ Weather/app/src/main/java/com/romanov/weather/adapter/TabsPagerFragmentAdapter.java
@@ -0,0 +1,48 @@
+package com.romanov.weather.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.romanov.weather.fragment.ExempleFragment;
+
+/**
+ * Created by Геннадий on 19.04.2016.
+ */
+public class TabsPagerFragmentAdapter extends FragmentPagerAdapter {
+
+    private String[] tabs;
+
+    public TabsPagerFragmentAdapter(FragmentManager fm) {
+        super(fm);
+        tabs = new String[]{
+                "Сейчас",
+                "Сегодня",
+                "Завтра"
+        };
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabs[position];
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return ExempleFragment.getInstance();
+            case 1:
+                return ExempleFragment.getInstance();
+            case 2:
+                return ExempleFragment.getInstance();
+
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return tabs.length;
+    }
+}
